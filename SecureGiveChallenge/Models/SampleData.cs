8bcaa049d1a2212fb39bbab90e5f38c3e77a69ee
Greg Johnson <greg2583@gmail.com>
2023-05-17 14:19:23 -0400

    Add project files.

---
+++ SecureGiveChallenge/Models/SampleData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace SecureGiveChallenge.Models
+{
+	public class SampleData
+	{
+		private static List<User> usersList = new List<User>();
+		public static IEnumerable<User> UsersList
+		{
+			get { return usersList; }
+		}
+		public static void Create(User newUser)
+		{
+			usersList.Add(newUser);
+		}
+	}
+}
---
+++ SecureGiveChallenge/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SecureGiveChallenge.Models
+{
+	public class User
+	{
+		public string? FirstName { get; set; }
+		public string? LastName { get; set; }
+		public Char UserType { get; set; }
+
+	}
+}
