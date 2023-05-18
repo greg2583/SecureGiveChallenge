0df936e1d3f06a027ce1d1b90e3efb801f737d07
Greg Johnson <greg2583@gmail.com>
2023-05-17 22:18:28 -0400

    2023-05-17 - Further updates in CRUD project

---
+++ SecureGiveChallenge/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SecureGiveChallenge.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecureGiveChallenge.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(SampleData.UsersList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            SampleData.Create(user);
+            return View("Thanks", user);
+        }
+
+
+    }
+}
---
+++ SecureGiveChallenge/Views/User/Thanks.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SecureGiveChallenge.Views.User
+{
+    public class ThanksModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
